06e86fbd02fb0a79f3971cd11244fff534e8969b
Yeong-Ung Kim <yeongungkim@gm.gist.ac.kr>
2025-12-23 19:56:51 +0900

    a

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,15 +14,15 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme']
+extensions = []
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = []
 
-language = 'ko'
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
