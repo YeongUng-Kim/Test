9213130b62ba464f1c0fdd894939d3f0213377eb
Yeong-Ung Kim <yeongungkim@gm.gist.ac.kr>
2025-12-23 19:16:26 +0900

    rst test

--- docs/conf.py
+++ docs/conf.py
@@ -3,28 +3,23 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-source_suffix = '.rst'
-master_doc = 'index'
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'TestDocs'
-copyright = '2025, Yeong-Ung Kim'
-author = 'Yeong-Ung Kim'
+copyright = '2025, Yeong-Ung'
+author = 'Yeong-Ung'
+release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.mathjax',
-              'sphinx.ext.viewcode',
-              'sphinx.ext.napoleon',
-              'sphinx.ext.intersphinx',
-              'sphinx_autodoc_typehints']
+extensions = ['sphinx_rtd_theme']
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'ko'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. TestDocs documentation master file, created by
-   sphinx-quickstart on Tue Dec 23 18:49:08 2025.
+   sphinx-quickstart on Tue Dec 23 19:14:25 2025.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
